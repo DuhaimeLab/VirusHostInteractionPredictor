b26272524589c934ebf445aa994e8cdeb9c6a878
Eric Bastien <gbastien@umich.edu>
2023-01-11 11:03:03 -0500

    added main file and skeleton functions

---
+++ VIP/predict_interactions.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+def load_pairs(path):
+    pairs = pd.read_csv(path)
+
+    # add code to check input is formatted correctly for prediction
+
+
+def predict_interactions():
+
+    pass
+
+
+
