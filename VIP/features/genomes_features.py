570522c6442fb7763b50279af34d443b798b3553
Eric Bastien <gbastien@umich.edu>
2023-01-18 11:12:21 -0500

    added skeleton functions for genomes features

---
+++ VIP/features/genomes_features.py
@@ -0,0 +1,20 @@
+'''
+
+
+'''
+
+
+def kmer_d2star_distance(prof1, prof2):
+    # calculate kmer distance between two profiles
+    pass
+
+
+def GC_difference(prof1, prof2):
+    # compute the GC difference between two profiles
+    pass
+
+
+def blastn_match(virus, host):
+    # determine if there is a match between virus and host
+    pass
+
