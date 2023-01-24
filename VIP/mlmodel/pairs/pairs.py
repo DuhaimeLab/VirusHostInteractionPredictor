63fc7a41231bfc580d13c0b702b4e7e5c68d68b9
Eric Bastien <gbastien@umich.edu>
2023-01-24 14:56:18 -0500

    used dataclass decorator for object pair

--- VIP/mlmodel/pairs/pairs.py
+++ VIP/mlmodel/pairs/pairs.py
@@ -2,21 +2,23 @@
 Description goes here
 '''
 
+from dataclasses import dataclass
 
+@dataclass
 class Pairs:
 
-    def __init__(self, virus, host) -> None:
-        
-        self.virus = virus
-        self.host = host
+    virus: str
+    host: str
+    interaction: int
 
-        # features
-        self.GCdiff = None
-        self.k3dist = None
-        self.k6dist = None
-        
-        self.spacers = False
-        self.blastn = False
-    
+    # features
+    GCdifference: float
+    k3dist: float
+    k6dist: float
+
+    spacers_hit: bool = False
+    blastn_hit: bool = False
+
+
     
 
