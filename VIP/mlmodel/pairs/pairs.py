41c2dd4660ddbb6c3e0e40a4fceeeb49e70d6dce
Eric Bastien <gbastien@umich.edu>
2023-01-28 18:43:37 -0500

    added functions to determine all pairs to consider

--- VIP/mlmodel/pairs/pairs.py
+++ VIP/mlmodel/pairs/pairs.py
@@ -3,6 +3,9 @@ Description goes here
 '''
 
 from dataclasses import dataclass
+import itertools
+import os
+
 
 @dataclass
 class Pairs:
@@ -21,5 +24,24 @@ class Pairs:
 
     # TODO: gene level features
 
-    
+
+
+def determine_pairs(virus_directory, host_directory):
+    virus_filenames = list_files(virus_directory)
+    host_filenames = list_files(host_directory)
+
+    total_interactions = len(virus_filenames) * len(host_filenames)
+    print(f'There are {len(virus_filenames)} viral sequences')
+    print(f'There are {len(host_filenames)} host sequences')
+    print(f'Total number of interactions: {total_interactions}')
+
+    virus_inter = list(itertools.chain.from_iterable(itertools.repeat(x, len(host_filenames)) for x in virus_filenames))
+    host_inter = host_filenames * len(virus_filenames)
+    return list(zip(virus_inter, host_inter))
+
+
+
+def list_files(path, ext='fasta'):
+    return [f for f in os.listdir(path) if f.endswith('.' + ext)]
+
 
