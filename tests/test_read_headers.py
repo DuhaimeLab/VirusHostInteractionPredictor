6f3567f695468528c3cc255e8a3776749f7bb46a
Nathan Fallahi <4059671+nashafa@users.noreply.github.com>
2023-11-17 12:24:45 -0500

    Minor formatting and refactoring that was missed

--- tests/test_read_headers.py
+++ tests/test_read_headers.py
@@ -1,7 +1,8 @@
-from vip.mlmodel.read_sequence import read_headers
+from vhip.mlmodel.read_sequence import read_headers
+
 
 def test_read_headers():
-    headers = ['NZ_CP065712.1']
-    filename = 'tests/datatests/test_sequence.fasta'
+    headers = ["NZ_CP065712.1"]
+    filename = "tests/datatests/test_sequence.fasta"
     res = read_headers(filename)
     assert res == headers
