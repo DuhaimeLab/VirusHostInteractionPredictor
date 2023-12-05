92c05a2e1e55cfe3c26e298f2d339e3ef4c1ded2
Eric Bastien <gbastien@umich.edu>
2023-12-05 11:12:59 -0500

    doc and linter for read_sequence module (getting reportMissingTybeStubs)

--- src/vhip/mlmodel/read_sequence.py
+++ src/vhip/mlmodel/read_sequence.py
@@ -1,16 +1,36 @@
+'''Utility functions to read fasta sequences.
+
+This module provides:
+- read_sequence: read fasta file using path as input
+- read_headers: retrieve the headers for fasta path
+'''
+
 from Bio import SeqIO
 
 
-def read_sequence(path):
-    """Return the sequence as a string.
-    Take path where file reside as input.
-    """
+def read_sequence(path: str) -> str:
+    '''Return the sequence in a fasta file as a string.
+
+    Args:
+        path (str): path to fasta file
+
+    Returns:
+        str: nucleotide sequence
+    '''
     for record in SeqIO.parse(path, "fasta"):
         return str(record.seq)
+    return 'no sequence for given path file'
 
 
-def read_headers(path):
-    """ """
+def read_headers(path: str) -> list[str]:
+    '''Return all headers in fasta file as a list.
+
+    Args:
+        path (str): path to fasta file
+
+    Returns:
+        list: list of all headers for a given fasta file
+    '''
     result = []
     for record in SeqIO.parse(path, "fasta"):
         result.append(record.id)
