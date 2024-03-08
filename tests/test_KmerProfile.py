e6dbf8047564f8d3c6d8ee7a07bb6d37fb74a36c
Nathan Fallahi <4059671+nashafa@users.noreply.github.com>
2024-03-08 08:14:17 -0500

    Sort imports and expand pyright coverage
    This adds the isort (I) set of rules to Ruff. Now, running ruff --fix should automatically sort imports.
    
    This also removes the restriction of pyright to the src folder, so files in tests and others will also be checked.

--- tests/test_KmerProfile.py
+++ tests/test_KmerProfile.py
@@ -1,6 +1,9 @@
 """Pytest for genomes_features module."""
-import pytest
+from typing import List
+
 import numpy as np
+import pytest
+
 from vhip.mlmodel.genomes_features import KmerProfile
 
 
@@ -84,7 +87,7 @@ def test_KmerProfile_generate_profile():
 def test_KmerProfile_empty_string():
     """Test for empty string as input to KmerProfile."""
     # Empty string given as input
-    seq = []
+    seq: List[str] = []
     with pytest.raises(ValueError) as excinfo:
         profile = KmerProfile(seq, 3)
         profile.generate_profile()
