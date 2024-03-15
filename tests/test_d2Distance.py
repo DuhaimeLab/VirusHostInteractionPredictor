5e9f5185d36ce081c997fc934e8a99c9819079fa
Eric Bastien <gbastien@umich.edu>
2024-03-15 15:22:45 -0400

    cleanup test file

--- tests/test_d2Distance.py
+++ tests/test_d2Distance.py
@@ -17,7 +17,6 @@ def test_d2Distance_distance():
 
     test = d2Distance(seq1_profile, seq2_profile)
     test.distance()
-    print(test.dist)
     dist = 0.5004  # distance for the two small sequences above
     assert np.round(test.dist, 4) == dist  # pyright: ignore[reportGeneralTypeIssues, reportCallIssue, reportArgumentType]
 
@@ -47,7 +46,3 @@ def test_d2Distance_distance():
     test = d2Distance(seq1_profile, seq2_profile)
     test.distance()
     assert test.dist is None
-
-
-if __name__ == "__main__":
-    test_d2Distance_distance()
