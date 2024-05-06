aaeb5f1d36b2d3bb1d7c8fa3be29285db8ed3d59
Evelyn Faust <evfaust@umich.edu>
2024-05-06 14:05:43 -0400

    test Gene object initialization
    check that seq and codon_length attributes are correct

--- tests/test_Gene.py
+++ tests/test_Gene.py
@@ -1,3 +1,12 @@
 """Pytest for Gene methods in gene_features module."""
 
 from vhip.mlmodel.gene_features import Gene
+
+
+def test_Gene_attributes():
+    """Test code to create Gene object and initialize class attributes."""
+    test_gene = Gene("ATGCCGATT")
+    assert test_gene.seq == "ATGCCGATT"
+    assert test_gene.codon_length == 3
+
+
