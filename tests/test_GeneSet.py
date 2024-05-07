318e90793b08c7febe27ced3e9d3a63b30ad4471
Evelyn Faust <evfaust@umich.edu>
2024-05-07 09:46:33 -0400

    test GeneSet initialization
    check correct class variables (genes, gene_ids, gene_products) and Exception raised when provided genes file is empty

---
+++ tests/test_GeneSet.py
@@ -0,0 +1,22 @@
+"""Pytest for GeneSet methods in gene_features module."""
+
+import pytest
+
+from vhip.mlmodel.gene_features import GeneSet
+
+
+def test_GeneSet_init():
+    """Test code to create GeneSet object and initialize class attributes."""
+    # test 1 - test GeneSet object creation generates expected class attributes
+    test_GeneSet = GeneSet("tests/datatests/test_annotated_genes.ffn")
+    assert len(test_GeneSet.genes) == 3
+    assert test_GeneSet.gene_ids == ["ABDEAL_00005", "ABDEAL_00010", "ABDEAL_00015"]
+    assert test_GeneSet.gene_products == [
+        "Chromosomal replication initiator protein DnaA",
+        "Beta sliding clamp",
+        "S4 domain-containing protein YaaA",
+    ]
+
+    # test 2 - test Exception is raised if gene file is empty
+    with pytest.raises(Exception):
+        GeneSet("tests/datatests/test_empty_file.ffn")
