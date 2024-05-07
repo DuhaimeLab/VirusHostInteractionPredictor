c6893249ff0c06b63cec8f6ac7e5ca93ed6e51ad
Evelyn Faust <evfaust@umich.edu>
2024-05-07 14:30:17 -0400

    update test_GeneSet_init
    to remove obsolete attributes and test new attributes tied to Gene objects

--- tests/test_GeneSet.py
+++ tests/test_GeneSet.py
@@ -7,16 +7,15 @@ from vhip.mlmodel.gene_features import GeneSet
 
 def test_GeneSet_init():
     """Test code to create GeneSet object and initialize class attributes."""
-    # test 1 - test GeneSet object creation generates expected class attributes
+    # test 1 - test GeneSet object creation generates expected number of genes in genes attribute
     test_GeneSet = GeneSet("tests/datatests/test_annotated_genes.ffn")
     assert len(test_GeneSet.genes) == 3
-    assert test_GeneSet.gene_ids == ["ABDEAL_00005", "ABDEAL_00010", "ABDEAL_00015"]
-    assert test_GeneSet.gene_products == [
-        "Chromosomal replication initiator protein DnaA",
-        "Beta sliding clamp",
-        "S4 domain-containing protein YaaA",
-    ]
 
-    # test 2 - test Exception is raised if gene file is empty
+    # test 2 - test GeneSet object creation generates expected gene attributes for a gene
+    assert test_GeneSet.genes[2].seq == "TTGGTTGAAGAAGTAGTTGTAGATGGCGACATCACATTAGGACAATTTCTAAAGACGGAAGGTATTATCGAATCTGGCGGGCAAGCGAAATGGTTCTTAAATGAGTTTGAAGTATTGTTAAACAATACGCGTGAAACACGCCGTGGTAAAAAGTTAAGCCATCGTGACACAATTGAGATACCAGAAATACCTGAAGTGGGTTCATTTGTGATTTTGCATCAAGGTGAAGAATGA"
+    assert test_GeneSet.genes[2].gene_id == "ABDEAL_00015"
+    assert test_GeneSet.genes[2].gene_product == "S4 domain-containing protein YaaA"
+
+    # test 3 - test Exception is raised if gene file is empty
     with pytest.raises(Exception):
         GeneSet("tests/datatests/test_empty_file.ffn")
