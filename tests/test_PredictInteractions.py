97cf26c07da87c697b274734d0a023fbab949665
Evelyn Faust <evfaust@umich.edu>
2024-06-28 22:28:19 -0400

    add test gene file directories - test_PredicInteractions
    and update PredictInteractions initialization to include them as args

--- tests/test_PredictInteractions.py
+++ tests/test_PredictInteractions.py
@@ -5,6 +5,8 @@ from vhip.predict_interactions import PredictInteractions
 
 test_virus_genome_dir = "tests/datatests/sequences/virus_genomes/"
 test_host_genome_dir = "tests/datatests/sequences/host_genomes/"
+test_virus_gene_dir = "tests/datatests/sequences/virus_genes"
+test_host_gene_dir = "tests/datatests/sequences/host_genes"
 ml_training = "tests/datatests/ml_input.csv"
 
 
@@ -13,7 +15,7 @@ def test_PredictInteractions_complete_pipeline():
     model = BuildModel(ml_training)
     VHIP = model.build()
 
-    test = PredictInteractions(test_virus_genome_dir, test_host_genome_dir)
+    test = PredictInteractions(test_virus_genome_dir, test_host_genome_dir, test_virus_gene_dir, test_host_gene_dir)
     test.model = VHIP
     test.add_blastn_files(
         "tests/datatests/blastn_phagevhost.tsv",
