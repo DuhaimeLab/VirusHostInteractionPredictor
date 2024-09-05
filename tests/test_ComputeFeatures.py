f12592498ab6a479ff80907499376c131f785a8b
Evelyn Faust <evfaust@umich.edu>
2024-09-05 01:13:03 -0400

    add test of correct codon frq calculation to generate_codon_frq test
    and update docstrings

--- tests/test_ComputeFeatures.py
+++ tests/test_ComputeFeatures.py
@@ -189,7 +189,7 @@ def test_ComputeFeatures_generate_kmer_profiles():
 
 
 def test_ComputeFeatures_generate_codon_frq():
-    """Test the codon frequency profiles are properly generated."""
+    """Test the codon frequency profiles are properly generated with expected values."""
     test = ComputeFeatures(
         test_virus_genome_dir,
         test_host_genome_dir,
@@ -204,6 +204,72 @@ def test_ComputeFeatures_generate_codon_frq():
     assert isinstance(
         test.codon_counts["GCA_003344205.1_ASM334420v1_genomic.ffn"], dict
     )
+    assert test.codon_frqs["test_short_genes_file.ffn"] == {
+        "ATA": 0.0,
+        "ATC": 0.0,
+        "ATT": 0.0,
+        "ATG": 1 / 4,
+        "ACA": 0.0,
+        "ACC": 0.0,
+        "ACG": 0.0,
+        "ACT": 0.0,
+        "AAC": 0.0,
+        "AAT": 0.0,
+        "AAA": 0.0,
+        "AAG": 0.0,
+        "AGC": 0.0,
+        "AGT": 0.0,
+        "AGA": 0.0,
+        "AGG": 0.0,
+        "CTA": 0.0,
+        "CTC": 0.0,
+        "CTG": 0.0,
+        "CTT": 0.0,
+        "CCA": 0.0,
+        "CCC": 0.0,
+        "CCG": 0.0,
+        "CCT": 0.0,
+        "CAC": 0.0,
+        "CAT": 0.0,
+        "CAA": 0.0,
+        "CAG": 0.0,
+        "CGA": 0.0,
+        "CGC": 0.0,
+        "CGG": 0.0,
+        "CGT": 0.0,
+        "GTA": 0.0,
+        "GTC": 0.0,
+        "GTG": 0.0,
+        "GTT": 0.0,
+        "GCA": 0.0,
+        "GCC": 0.0,
+        "GCG": 0.0,
+        "GCT": 0.0,
+        "GAC": 0.0,
+        "GAT": 0.0,
+        "GAA": 1 / 4,
+        "GAG": 0.0,
+        "GGA": 0.0,
+        "GGC": 0.0,
+        "GGG": 0.0,
+        "GGT": 0.0,
+        "TCA": 1 / 4,
+        "TCC": 1 / 4,
+        "TCG": 0.0,
+        "TCT": 0.0,
+        "TTC": 0.0,
+        "TTT": 0.0,
+        "TTA": 0.0,
+        "TTG": 0.0,
+        "TAC": 0.0,
+        "TAT": 0.0,
+        "TAA": 0.0,
+        "TAG": 0.0,
+        "TGC": 0.0,
+        "TGT": 0.0,
+        "TGA": 0.0,
+        "TGG": 0.0,
+    }
 
 
 def test_ComputeFeatures_complete_pipeline():
@@ -225,14 +291,3 @@ def test_ComputeFeatures_complete_pipeline():
     assert test.features_df.shape[0] == 12
     assert test.features_df.shape[1] == 4
 
-if __name__ == "__main__":
-    test = ComputeFeatures(
-        test_virus_genome_dir,
-        test_host_genome_dir,
-        test_virus_gene_dir,
-        test_host_gene_dir,
-    )
-    test.list_genome_files()
-    test.determine_pairs()
-    test.generate_kmer_profiles()
-    print(test.k6profiles["GCA_003344205.1_ASM334420v1_genomic.fasta"])
