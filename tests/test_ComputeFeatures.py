c248ddfbfa3ccbc79e09bfa67b1e4f2105928958
Evelyn Faust <evfaust@umich.edu>
2024-09-28 12:45:53 -0400

    create pytest for generate_codon_aa_counts

--- tests/test_ComputeFeatures.py
+++ tests/test_ComputeFeatures.py
@@ -188,6 +188,116 @@ def test_ComputeFeatures_generate_kmer_profiles():
     )
 
 
+def test_ComputeFeatures_generate_codon_aa_counts():
+    """Test the codon and amino acid counts are properly generated with expected values."""
+    test = ComputeFeatures(
+        test_virus_genome_dir,
+        test_host_genome_dir,
+        test_virus_gene_dir,
+        test_host_gene_dir,
+    )
+    test.list_gene_files()
+    test.generate_codon_aa_counts()
+    assert isinstance(test.codon_counts, dict)
+    assert isinstance(test.aa_counts, dict)
+    assert isinstance(
+        test.codon_counts["GCA_003344205.1_ASM334420v1_genomic.ffn"], dict
+    )
+    assert isinstance(
+        test.aa_counts["GCA_003344205.1_ASM334420v1_genomic.ffn"], dict
+    )
+    assert test.codon_counts["test_short_genes_file.ffn"] == {
+        "ATA": 0,
+        "ATC": 0,
+        "ATT": 0,
+        "ATG": 1,
+        "ACA": 0,
+        "ACC": 0,
+        "ACG": 0,
+        "ACT": 0,
+        "AAC": 0,
+        "AAT": 0,
+        "AAA": 0,
+        "AAG": 0,
+        "AGC": 0,
+        "AGT": 0,
+        "AGA": 0,
+        "AGG": 0,
+        "CTA": 0,
+        "CTC": 0,
+        "CTG": 0,
+        "CTT": 0,
+        "CCA": 0,
+        "CCC": 0,
+        "CCG": 0,
+        "CCT": 0,
+        "CAC": 0,
+        "CAT": 0,
+        "CAA": 0,
+        "CAG": 0,
+        "CGA": 0,
+        "CGC": 0,
+        "CGG": 0,
+        "CGT": 0,
+        "GTA": 0,
+        "GTC": 0,
+        "GTG": 0,
+        "GTT": 0,
+        "GCA": 0,
+        "GCC": 0,
+        "GCG": 0,
+        "GCT": 0,
+        "GAC": 0,
+        "GAT": 0,
+        "GAA": 1,
+        "GAG": 0,
+        "GGA": 0,
+        "GGC": 0,
+        "GGG": 0,
+        "GGT": 0,
+        "TCA": 1,
+        "TCC": 1,
+        "TCG": 0,
+        "TCT": 0,
+        "TTC": 0,
+        "TTT": 0,
+        "TTA": 0,
+        "TTG": 0,
+        "TAC": 0,
+        "TAT": 0,
+        "TAA": 0,
+        "TAG": 0,
+        "TGC": 0,
+        "TGT": 0,
+        "TGA": 0,
+        "TGG": 0,
+    }
+
+    assert test.aa_counts["test_short_genes_file.ffn"] == {
+        "I": 0,
+        "M": 1,
+        "T": 0,
+        "N": 0,
+        "K": 0,
+        "S": 2,
+        "R": 0,
+        "L": 0,
+        "P": 0,
+        "H": 0,
+        "Q": 0,
+        "V": 0,
+        "A": 0,
+        "D": 0,
+        "E": 1,
+        "G": 0,
+        "F": 0,
+        "Y": 0,
+        "_": 0,
+        "C": 0,
+        "W": 0,
+    }
+
+
 def test_ComputeFeatures_generate_codon_frq():
     """Test the codon frequency profiles are properly generated with expected values."""
     test = ComputeFeatures(
