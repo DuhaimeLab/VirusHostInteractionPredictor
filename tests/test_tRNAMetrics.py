93096ceb086b3c612358c89b73b680f1a1911bdf
Evelyn Faust <evfaust@umich.edu>
2025-03-07 20:57:26 -0600

    create test_tRNA_Metric_init test

--- tests/test_tRNAMetrics.py
+++ tests/test_tRNAMetrics.py
@@ -2,4 +2,15 @@
 
 import pytest
 
-from vhip.mlmodel.gene_features import tRNAMetrics
+from vhip.mlmodel.gene_features import GeneSet,tRNAMetrics
+
+virus_geneset = GeneSet("tests/datatests/test_virus_tRNA_genes.ffn")
+host_geneset = GeneSet("tests/datatests/test_host_tRNA_genes.ffn")
+
+def test_tRNAMetrics_init():
+    """Test code to create tRNAMetrics object and initialize class attributes."""
+    test_tRNAMetrics = tRNAMetrics(virus_geneset, host_geneset)
+    assert hasattr(test_tRNAMetrics, "virus_GeneSet")
+    assert hasattr(test_tRNAMetrics, "host_GeneSet")
+    assert hasattr(test_tRNAMetrics.virus_GeneSet, "tRNA_frq_aa")
+    assert hasattr(test_tRNAMetrics.host_GeneSet, "tRNA_frq_aa")
