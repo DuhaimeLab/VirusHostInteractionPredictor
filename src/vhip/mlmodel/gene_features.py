b2a6a40e0b50264d75442649986e8063ae56b2d4
Evelyn Faust <evfaust@umich.edu>
2025-03-26 23:03:22 -0500

    address another potential key error in the tcc tRNA dict
    action for future: dig into Us in tcc inputs

--- src/vhip/mlmodel/gene_features.py
+++ src/vhip/mlmodel/gene_features.py
@@ -443,11 +443,12 @@ class GeneSet:
                 has_tRNAs = True
                 aa_3 = gene.gene_product.split("-")[1].split("(")[0]
                 if aa_3 in AA_CONVERSIONS.keys():
-                    aa_1 = AA_CONVERSIONS[aa_3] #new
+                    aa_1 = AA_CONVERSIONS[aa_3] #new (SeC)
                     self.tRNA_dict_aa[aa_1] += 1
                     anticodon = gene.gene_product.split("(")[1].split(")")[0]
                     tcc = reverse_complement(anticodon)
-                    self.tRNA_dict_tcc[tcc] += 1
+                    if tcc in self.tRNA_dict_tcc.keys(): #new (UGG)
+                        self.tRNA_dict_tcc[tcc] += 1
 
         # Calculate total tRNA count
         self.total_tRNA: int = sum(self.tRNA_dict_tcc.values())
