ff10f30abbc7fec5898e6f8c1ae0abf692304dba
Evelyn Faust <evfaust@umich.edu>
2025-04-20 15:21:55 -0500

    working CodonBiasComparison init pytest
    updated for .json inputs

--- tests/test_CodonBiasComparison.py
+++ tests/test_CodonBiasComparison.py
@@ -9,18 +9,14 @@ from vhip.mlmodel.gene_features import CodonBiasComparison, GeneSet
 def test_CodonBiasComparison_init():
     """Test code to create CodonBiasComparison object and initialize class attributes."""
     # Create host GeneSet object and calculate codon bias
-    test_host_GeneSet = GeneSet("tests/datatests/test_short_genes_file.ffn")
-    test_host_GeneSet.codon_counts()
+    test_host_GeneSet = GeneSet("tests/datatests/test_short_genes.json")
     test_host_GeneSet.codon_frequency()
-    test_host_GeneSet.amino_acid_counts()
     test_host_GeneSet.amino_acid_frequency()
     test_host_GeneSet.RSCU()
 
     # Create virus GeneSet object and calculate codon bias
-    test_virus_GeneSet = GeneSet("tests/datatests/test_virus_short_genes_file.ffn")
-    test_virus_GeneSet.codon_counts()
+    test_virus_GeneSet = GeneSet("tests/datatests/test_virus_short_genes.json")
     test_virus_GeneSet.codon_frequency()
-    test_virus_GeneSet.amino_acid_counts()
     test_virus_GeneSet.amino_acid_frequency()
     test_virus_GeneSet.RSCU()
 
