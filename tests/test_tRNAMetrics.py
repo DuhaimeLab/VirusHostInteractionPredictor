d0fad16c7e86220c91b6332cb73d518887041a9a
Evelyn Faust <evfaust@umich.edu>
2025-04-27 17:15:55 -0500

    update test_tRNAMetrics pytest for TAAI
    to accommodate new dict inputs

--- tests/test_tRNAMetrics.py
+++ tests/test_tRNAMetrics.py
@@ -6,48 +6,55 @@ import scipy  # pyright: ignore[reportMissingTypeStubs]
 
 from vhip.mlmodel.gene_features import GeneSet, tRNAMetrics
 
-virus_geneset = GeneSet("tests/datatests/test_virus_tRNA_genes.ffn")
-host_geneset = GeneSet("tests/datatests/test_host_tRNA_genes.ffn")
+# Set up test data
+host_GS = GeneSet("tests/datatests/test_tRNA_genes.json")
+virus_GS = GeneSet("tests/datatests/test_short_genes.json")
 
+virus_GS.amino_acid_frequency()
+virus_GS.tRNA_counts()
+host_GS.tRNA_counts()
+
+virus_aa_frq = virus_GS.aa_frq
+host_tRNA_dict_aa = host_GS.tRNA_dict_aa_1_letter
+virus_tRNA_dict_aa = virus_GS.tRNA_dict_aa_1_letter
 
 def test_tRNAMetrics_init():
     """Test code to create tRNAMetrics object and initialize class attributes."""
-    test_tRNAMetrics = tRNAMetrics(virus_geneset, host_geneset)
-    assert hasattr(test_tRNAMetrics, "virus_GeneSet")
-    assert hasattr(test_tRNAMetrics, "host_GeneSet")
-    assert hasattr(test_tRNAMetrics.virus_GeneSet, "tRNA_frq_aa")
-    assert hasattr(test_tRNAMetrics.host_GeneSet, "tRNA_frq_aa")
-
 
 def test_tRNAMetrics_virus_TAAI():
     """Test code to calculate virus amino acid accordance with tRNA availability."""
-    test_tRNAMetrics = tRNAMetrics(virus_geneset, host_geneset)
-    test_tRNAMetrics.virus_TAAI()
+    test_tRNAMetrics = tRNAMetrics()
+    test_tRNAMetrics.virus_TAAI(virus_aa_frq=virus_aa_frq, host_tRNA_dict_aa=host_tRNA_dict_aa, virus_tRNA_dict_aa=virus_tRNA_dict_aa)
 
-    # test 1: check that amino acid frequency has been calculated for virus GeneSet attribute
-    assert hasattr(test_tRNAMetrics.virus_GeneSet, "aa_frq")
-
-    # test 2: check TAAI logic (spearman rank between identical data should be 1, and between opposite data should be -1)
-    data = list(test_tRNAMetrics.host_GeneSet.tRNA_frq_aa.values())
+    # test 1: check TAAI logic (spearman rank between identical data should be 1, and between opposite data should be -1)
+    sorted_keys = sorted(virus_aa_frq)
+    data = [host_tRNA_dict_aa[key] for key in sorted_keys]
     res1 = scipy.stats.spearmanr(data, data)
     assert math.isclose(res1.statistic, 1.0, rel_tol=1e-6)
-    # res2 = scipy.stats.spearmanr(data, data[::-1])            ### mystery pytest issue - logic works locally
-    # assert math.isclose(res2.statistic, -1.0, rel_tol=1e-6)   ### mystery pytest issue - logic works locally
 
-    # test 3: check correct correlation coefficient between virus amino acid frequency and host tRNA frequency
+    # test 2: check correct correlation coefficient between virus amino acid frequency and host tRNA frequency
     assert math.isclose(
-        test_tRNAMetrics.virusTAAI_hosttRNA, 0.7814187052403264, rel_tol=1e-6
+        test_tRNAMetrics.virusTAAI_hosttRNA, -0.1972026594366539, rel_tol=1e-6
     )
 
-    # test 4: check correct correlation coefficient between virus amino acid frequency and TOTAL tRNA frequency
+    # test 3: check correct correlation coefficient between virus amino acid frequency and TOTAL tRNA frequency
     assert math.isclose(
-        test_tRNAMetrics.virusTAAI_totaltRNA, 0.7814187052403264, rel_tol=1e-6
+        test_tRNAMetrics.virusTAAI_totaltRNA, 0.07562376774775251, rel_tol=1e-6
     )
 
-    # test 5: check no total tRNA comparison metric is generated if parameter specified as false
-    test5_tRNAMetrics = tRNAMetrics(virus_geneset, host_geneset)
-    test5_tRNAMetrics.virus_TAAI(include_virus_tRNA=False)
-    assert not hasattr(test5_tRNAMetrics, "virusTAAI_totaltRNA")
+    # test 4: check no total tRNA comparison metric is generated if no virus tRNA dict provided
+    test4_tRNAMetrics = tRNAMetrics()
+    test4_tRNAMetrics.virus_TAAI(virus_aa_frq=virus_aa_frq, host_tRNA_dict_aa=host_tRNA_dict_aa)
+    assert not hasattr(test4_tRNAMetrics, "virusTAAI_totaltRNA")
+
+
+
+
+
+
+
+
+
 
 
 def test_tRNAMetrics_virus_TCAI():
